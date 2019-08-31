599ff714e49f471722e31ac0542ebfb3271ae99f
main desktop <da.ga.mo.94@gmail.com>
2019-08-31 00:57:09 -0600

    several changes to all pages. Info added, styling, etc.

--- js/landing-page.js
+++ js/landing-page.js
@@ -4,3 +4,10 @@ $(function () {
         $nav.toggleClass('scrolled', $(this).scrollTop() > ($(".carousel-item").height() - $nav.height() * 2));
     });
 });
+
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $(".contact-nav");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > (10 - $nav.height() * 2));
+    });
+});
